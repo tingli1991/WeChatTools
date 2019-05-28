8af877c8859ea65f7725956e964414409e681088
Gemgin <gemgin@126.com>
2019-05-28 10:11:44 +0800

    短链接生成:一个key不论成功与否,一天只能调用5000次

--- WeChatTools/WeChatTools.API/pro/shorturl.ashx.cs
+++ WeChatTools/WeChatTools.API/pro/shorturl.ashx.cs
@@ -17,15 +17,13 @@ namespace WeChatTools.API
     public class shorturl : IHttpHandler
     {
         private const int DURATION = 24 * 60;
-        private static string userIP = "127.0.0.1";
-        // private string shortApiKey = string.Empty;
+        
         protected const string GET = "GET";
         public void ProcessRequest(HttpContext context)
         {
             string result = string.Empty;
             if (context.Request.HttpMethod.ToUpper().Equals(GET))
             {
-
                 string url = context.Request["url"];
                 string key = context.Request["key"]; //key ,md5值
                 string type = context.Request["type"]; //key ,md5值
@@ -33,46 +31,46 @@ namespace WeChatTools.API
 
                 if (!string.IsNullOrEmpty(url) && !string.IsNullOrEmpty(key) && key.Length == 32)
                 {
-                    ServiceApiClient SpVoiceObj2 = null;
-                    //  ServiceApiClient SpVoiceObj = null;
-                    try
+                    if (!IsRedis(context, key))
                     {
-                        if (!url.Contains("%3a%2f%2f")) { url = System.Web.HttpUtility.UrlEncode(url); }
-
-                        string json2 = "{\"Mode\":\"ShortUrl\",\"Param\":\"{\'CheckUrl\':\'" + url + "\',\'type\':\'" + type + "\',\'UserKey\':\'" + key + "\'}\"}";
-
-                        SpVoiceObj2 = new ServiceApiClient("NetTcpBinding_IServiceApi");
-                        SpVoiceObj2.Open();
-                        result = SpVoiceObj2.Api(json2);
-                        SpVoiceObj2.Close();
-
-
-                        if (!string.IsNullOrEmpty(context.Request.QueryString["callback"]))
+                        result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + key + "\",\"Msg\":\"当天请求上限,请明天再试,需要讨论技术,联系管理员qq:391502069!\"}";
+                    }
+                    else
+                    {
+                        ServiceApiClient SpVoiceObj = null;
+                        try
                         {
-                            string callBack = context.Request.QueryString["callback"].ToString(); //回调
-                            result = callBack + "(" + result + ")";
+                            if (!url.Contains("%3a%2f%2f")) { url = System.Web.HttpUtility.UrlEncode(url); }
+
+                            string json2 = "{\"Mode\":\"ShortUrl\",\"Param\":\"{\'CheckUrl\':\'" + url + "\',\'type\':\'" + type + "\',\'UserKey\':\'" + key + "\'}\"}";
+
+                            SpVoiceObj = new ServiceApiClient("NetTcpBinding_IServiceApi");
+                            SpVoiceObj.Open();
+                            result = SpVoiceObj.Api(json2);
+                            SpVoiceObj.Close();
+
+
+                            if (!string.IsNullOrEmpty(context.Request.QueryString["callback"]))
+                            {
+                                string callBack = context.Request.QueryString["callback"].ToString(); //回调
+                                result = callBack + "(" + result + ")";
+                            }
+                        }
+                        catch (System.ServiceModel.CommunicationException)
+                        {
+                            if (SpVoiceObj != null) SpVoiceObj.Abort();
+                        }
+                        catch (TimeoutException)
+                        {
+                            if (SpVoiceObj != null) SpVoiceObj.Abort();
+                        }
+                        catch (Exception ex)
+                        {
+                            if (SpVoiceObj != null) SpVoiceObj.Abort();
+                            result = "{\"State\":false,\"Code\",\"003\",\"Data\":\"" + url + "\",\"Msg\":\"请求操作在配置的超时,请联系管理员!\"}";
+                            LogTools.WriteLine( key + ":" + ex.Message);
                         }
                     }
-                    catch (System.ServiceModel.CommunicationException)
-                    {
-                        //   if (SpVoiceObj != null) SpVoiceObj.Abort();
-                        if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
-                    }
-                    catch (TimeoutException)
-                    {
-                        // if (SpVoiceObj != null) SpVoiceObj.Abort();
-                        if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
-                    }
-                    catch (Exception ex)
-                    {
-                        //正式用
-                        userIP = GetWebClientIp(context);
-                        //   if (SpVoiceObj != null) SpVoiceObj.Abort();
-                        if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
-                        result = "{\"State\":false,\"Code\",\"003\",\"Data\":\"" + url + "\",\"Msg\":\"请求操作在配置的超时,请联系管理员!\"}";
-                        LogTools.WriteLine(userIP + ":" + key + ":" + ex.Message);
-                    }
-
 
 
                 }
@@ -100,239 +98,28 @@ namespace WeChatTools.API
             }
         }
 
-        /// <summary>  
-        /// 将c# DateTime时间格式转换为Unix时间戳格式  
-        /// </summary>  
-        /// <param name="time">时间</param>  
-        /// <returns>long</returns>  
-        public static long ConvertDateTimeToInt()
-        {
-            System.DateTime time = DateTime.Now;
-            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
-            long t = (time.Ticks - startTime.Ticks) / 10000;   //除10000调整为13位      
-            return t;
-        }
 
-
-
-        /// <summary>  
-        /// 获取外网ip地址  
-        /// </summary>  
-        public static string GetExtenalIpAddress()
-        {
-            String url = "http://city.ip138.com/ip2city.asp";
-            string IP = "未获取到外网ip";
-            try
-            {
-                //从网址中获取本机ip数据    
-                System.Net.WebClient client = new System.Net.WebClient();
-                client.Encoding = System.Text.Encoding.Default;
-                string str = client.DownloadString(url);
-                client.Dispose();
-
-                //提取外网ip数据 [218.104.71.178]    
-                int i1 = str.IndexOf("["), i2 = str.IndexOf("]");
-
-                if (!str.Equals("")) IP = str.Substring(i1 + 1, i2 - 1 - i1);
-                else IP = GetExtenalIpAddress_0();
-            }
-            catch (Exception) { }
-
-            return IP;
-        }
-
-        /// <summary>  
-        /// 获取外网ip地址  
-        /// </summary>  
-        public static string GetExtenalIpAddress_0()
-        {
-            var tempIp = "";
-            try
-            {
-                WebRequest wr = (HttpWebRequest)WebRequest.Create("http://ip.chinaz.com/getip.aspx");
-                var stream = wr.GetResponse().GetResponseStream();
-                var sr = new StreamReader(stream, Encoding.GetEncoding("gb2312"));
-                var all = sr.ReadToEnd();
-                //读取网站的数据
-                int start = all.IndexOf("[") + 1;
-                int end = all.IndexOf("]", start);
-                tempIp = all.Substring(start, end - start);
-                sr.Close();
-                stream.Close();
-            }
-            catch
-            {
-                // ignored
-            }
-            return tempIp;
-        }
-
-
-        public static string GetWebClientIp(HttpContext httpContext)
-        {
-            string customerIP = "127.0.0.1";
-
-            if (httpContext == null || httpContext.Request == null || httpContext.Request.ServerVariables == null) return customerIP;
-
-            customerIP = httpContext.Request.ServerVariables["HTTP_CDN_SRC_IP"];
-
-            if (String.IsNullOrWhiteSpace(customerIP) || "unknown".Equals(customerIP.ToLower()))
-            {
-
-                customerIP = httpContext.Request.ServerVariables["Proxy-Client-IP"];
-            }
-            if (String.IsNullOrWhiteSpace(customerIP) || "unknown".Equals(customerIP.ToLower()))
-            {
-
-                customerIP = httpContext.Request.ServerVariables["WL-Proxy-Client-IP"];
-            }
-            /*
-            if (String.IsNullOrWhiteSpace(customerIP) || "unknown".Equals(customerIP.ToLower()))
-            {
-
-                customerIP = httpContext.Request.ServerVariables["HTTP_VIA"];
-            }
-            */
-            if (String.IsNullOrWhiteSpace(customerIP))
-            {
-
-                customerIP = httpContext.Request.ServerVariables["HTTP_CLIENT_IP"];
-                if (!String.IsNullOrWhiteSpace(customerIP) && customerIP.Contains(","))
-                {
-                    customerIP = customerIP.Split(new char[] { ',' })[0];
-                }
-            }
-
-            if (String.IsNullOrWhiteSpace(customerIP) || "unknown".Equals(customerIP.ToLower()))
-            {
-
-                customerIP = httpContext.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
-                if (!String.IsNullOrWhiteSpace(customerIP) && customerIP.Contains(","))
-                {
-                    string[] xx = customerIP.Split(new char[] { ',' });
-                    if (xx.Length > 1)
-                    {
-                        customerIP = xx[xx.Length - 2].Trim();
-                    }
-                    else
-                    {
-                        customerIP = xx[0];
-
-                    }
-                }
-            }
-            if (String.IsNullOrWhiteSpace(customerIP))
-            {
-
-                customerIP = httpContext.Request.ServerVariables["REMOTE_ADDR"];
-                if (!String.IsNullOrWhiteSpace(customerIP) && customerIP.Contains(","))
-                {
-                    customerIP = customerIP.Split(new char[] { ',' })[0];
-                }
-
-            }
-
-            if (!IsIP(customerIP))
-            {
-                customerIP = "127.0.0.1";
-            }
-            return customerIP;
-        }
-
-        public static string GetRealIP(string CustomerIP)
-        {
-            string result = String.Empty;
-
-            result = CustomerIP;
-
-            //可能有代理   
-            if (!string.IsNullOrWhiteSpace(result))
-            {
-                //没有"." 肯定是非IP格式  
-                if (result.IndexOf(".") == -1)
-                {
-                    result = null;
-                }
-                else
-                {
-                    //有",",估计多个代理.取第一个不是内网的IP.  
-                    if (result.IndexOf(",") != -1)
-                    {
-                        result = result.Replace(" ", string.Empty).Replace("\"", string.Empty);
-
-                        string[] temparyip = result.Split(",;".ToCharArray());
-
-                        if (temparyip != null && temparyip.Length > 0)
-                        {
-                            for (int i = 0; i < temparyip.Length; i++)
-                            {
-                                //找到不是内网的地址  
-                                if (IsIP(temparyip[i])
-                                    && temparyip[i].Substring(0, 3) != "10."
-                                    && temparyip[i].Substring(0, 7) != "192.168"
-                                    && temparyip[i].Substring(0, 7) != "172.16.")
-                                {
-                                    return temparyip[i];
-                                }
-                            }
-                        }
-                    }
-                    //代理即是IP格式  
-                    else if (IsIP(result))
-                    {
-                        return result;
-                    }
-                    //代理中的内容非IP  
-                    else
-                    {
-                        result = "";
-                    }
-                }
-            }
-
-
-            return result;
-        }
-
-        /// <summary>
-        /// 检查IP地址格式
-        /// </summary>
-        /// <param name="ip"></param>
-        /// <returns></returns>
-        public static bool IsIP(string ip)
-        {
-            if (!String.IsNullOrWhiteSpace(ip))
-            {
-                return System.Text.RegularExpressions.Regex.IsMatch(ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
-            }
-            else
-            {
-                return false;
-            }
-
-        }
-
-        public static bool IsValid(HttpContext context)
+        //每个key 24小时只能请求2000次
+        public static bool IsRedis(HttpContext context, string key)
         {
             if (context.Request.Browser.Crawler) return false;
-            string key = userIP;
-
-            int hit = (Int32)(context.Cache[key] ?? 0);
-            if (hit > 9) return false;
-            else hit++;
-
-            if (hit == 1)
+            key = "keycount:" + key;
+            bool check = RedisCacheTools.Exists(key);
+            if (check)
             {
-                context.Cache.Add(key, hit, null, DateTime.Now.AddMinutes(DURATION), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
-
+                RedisCacheTools.Incr(key);
+                int hit = RedisCacheTools.Get<int>(key);
+                if (hit > 5000) return false;
             }
             else
             {
-                context.Cache[key] = hit;
+                DateTime dt = DateTime.Now.AddDays(1);
+                RedisCacheTools.Incr(key);
+
+                RedisCacheTools.Expire(key, dt);
             }
+
             return true;
         }
-
-
     }
 }
