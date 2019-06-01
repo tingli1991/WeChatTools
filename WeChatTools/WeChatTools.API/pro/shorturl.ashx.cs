71c0caa9fa4f09c5a1f54578854972f8be2bed7d
Gemgin <gemgin@126.com>
2019-06-01 18:29:50 +0800

    修改bug

--- WeChatTools/WeChatTools.API/pro/shorturl.ashx.cs
+++ WeChatTools/WeChatTools.API/pro/shorturl.ashx.cs
@@ -40,7 +40,7 @@ namespace WeChatTools.API
                         ServiceApiClient SpVoiceObj = null;
                         try
                         {
-                            if (type=="WURLCN") { url = System.Web.HttpUtility.UrlEncode(url); }
+                            if (type.ToUpper() != "DWZCN") { url = System.Web.HttpUtility.UrlEncode(url); }
 
                             string json2 = "{\"Mode\":\"ShortUrl\",\"Param\":\"{\'CheckUrl\':\'" + url + "\',\'type\':\'" + type + "\',\'UserKey\':\'" + key + "\'}\"}";
                             
