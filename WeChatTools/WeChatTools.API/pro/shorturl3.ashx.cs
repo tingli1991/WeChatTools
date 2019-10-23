2ad07aa27a0c2eaac38ec7e87750496ca9f85776
Gemgin <gemgin@126.com>
2019-10-23 17:07:14 +0800

    短链接还原功能

--- WeChatTools/WeChatTools.API/pro/shorturl3.ashx.cs
+++ WeChatTools/WeChatTools.API/pro/shorturl3.ashx.cs
@@ -100,7 +100,7 @@ namespace WeChatTools.API.pro
             {
                 result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"https://url.cn/5mfnDv7\",\"Msg\":\"参数错误,联系管理员qq:391502069!\"}";
             }
-            context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            context.Response.Headers.Add("Access-Control-Allow-Origin", "http://www.rrbay.com");
             context.Response.Headers.Add("Access-Control-Allow-Methods", "POST");
             context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
 
