2cde8e33f03dffed32bab3506256e4bde085cf75
Gemgin <gemgin@126.com>
2019-10-25 17:11:12 +0800

    优化代码

--- WeChatTools/WeChatTools.API/pro/wxUrlCheck3.ashx.cs
+++ WeChatTools/WeChatTools.API/pro/wxUrlCheck3.ashx.cs
@@ -17,11 +17,10 @@ namespace WeChatTools.API.pro
         private const int DURATION = 24 * 60;
         private static string userIP = "127.0.0.1";
         protected const string POST = "POST";
-      
+
         private string wxKey = ConfigTool.ReadVerifyConfig("wxCheckApiKey3", "WeChatCheck");
-      
-        private TimeSpan _strWorkingDayAM = DateTime.Parse("08:00").TimeOfDay;//工作时间上午08:00
-        private TimeSpan _strWorkingDayPM = DateTime.Parse("21:00").TimeOfDay;
+
+
         public void ProcessRequest(HttpContext context)
         {
             string result = string.Empty;
@@ -34,91 +33,86 @@ namespace WeChatTools.API.pro
                 TimeSpan dspNow = DateTime.Now.TimeOfDay;
 
                 string urlCheck = context.Request["url"];
-             
+
                 string model = context.Request["model"];
-                if (IsInTimeInterval(dspNow, _strWorkingDayAM, _strWorkingDayPM))
+
+                // string referrer = context.Request.UrlReferrer != null ? context.Request.UrlReferrer.Host.ToLower() : "";
+
+                if (!string.IsNullOrEmpty(urlCheck) && !string.IsNullOrEmpty(model))
                 {
-                    // string referrer = context.Request.UrlReferrer != null ? context.Request.UrlReferrer.Host.ToLower() : "";
-                   
-                    if (!string.IsNullOrEmpty(urlCheck) && !string.IsNullOrEmpty(model))
-                    {                        
-                        if (!IsRedis(context))
-                        {
-                            result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + userIP + "\",\"Msg\":\"当天请求上限,请明天再试,需要讨论技术,联系管理员qq:391502069!\"}";
-                        }
-                        else
-                        {
-                            if (!string.IsNullOrEmpty(context.Request["key"]) && context.Request["key"].Length == 32)
-                            {
-                                wxKey = context.Request["key"]; //key ,md5值
-                            }
-
-
-                            ServiceApiClient SpVoiceObj2 = null;
-                            //    ServiceApiClient SpVoiceObj = null;
-                            try
-                            {
-                                //需要检测的网址                                
-                                bool isTrue = urlCheck.StartsWith("http");
-                                if (!isTrue) { urlCheck = "http://" + urlCheck; }
-                                urlCheck = System.Web.HttpUtility.UrlEncode(urlCheck);
-
-                                string json2 = "{\"Mode\":\"" + model + "\",\"Param\":\"{\'CheckUrl\':\'" + urlCheck + "\',\'UserKey\':\'" + wxKey + "\'}\"}";
-
-                                SpVoiceObj2 = new ServiceApiClient("NetTcpBinding_IServiceApi");
-                                SpVoiceObj2.Open();
-                                result = SpVoiceObj2.Api(json2);
-                                SpVoiceObj2.Close();
-                             
-
-                                Logger.WriteLogggerTest("#################################################");
-                                Logger.WriteLogggerTest(wxKey + ":" + userIP + ":" + result);
-                                Logger.WriteLogggerTest(wxKey + ":" + context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
-
-
-
-
-
-                            }
-                            catch (System.ServiceModel.CommunicationException)
-                            {
-                                //  if (SpVoiceObj != null) SpVoiceObj.Abort();
-                                if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
-                            }
-                            catch (TimeoutException)
-                            {
-                                //   if (SpVoiceObj != null) SpVoiceObj.Abort();
-                                if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
-                            }
-                            catch (Exception ex)
-                            {
-                                //   if (SpVoiceObj != null) SpVoiceObj.Abort();
-                                if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
-                                result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + urlCheck + "\",\"Msg\":\"请求操作在配置的超时,请联系管理员!\"}";
-                                LogTools.WriteLine(userIP + ":" + wxKey + ":" + ex.Message);
-                            }
-
-
-                        }
+                    if (!IsRedis(context))
+                    {
+                        result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + userIP + "\",\"Msg\":\"当天请求上限,请明天再试,需要讨论技术,联系管理员qq:391502069!\"}";
                     }
                     else
                     {
-                        result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + userIP + "\",\"Msg\":\"参数错误,联系管理员qq:391502069!\"}";
+                        if (!string.IsNullOrEmpty(context.Request["key"]) && context.Request["key"].Length == 32)
+                        {
+                            wxKey = context.Request["key"]; //key ,md5值
+                        }
+
+
+                        ServiceApiClient SpVoiceObj2 = null;
+                        //    ServiceApiClient SpVoiceObj = null;
+                        try
+                        {
+                            //需要检测的网址                                
+                            bool isTrue = urlCheck.StartsWith("http");
+                            if (!isTrue) { urlCheck = "http://" + urlCheck; }
+                            urlCheck = System.Web.HttpUtility.UrlEncode(urlCheck);
+
+                            string json2 = "{\"Mode\":\"" + model + "\",\"Param\":\"{\'CheckUrl\':\'" + urlCheck + "\',\'UserKey\':\'" + wxKey + "\'}\"}";
+
+                            SpVoiceObj2 = new ServiceApiClient("NetTcpBinding_IServiceApi");
+                            SpVoiceObj2.Open();
+                            result = SpVoiceObj2.Api(json2);
+                            SpVoiceObj2.Close();
+
+
+                            Logger.WriteLogggerTest("#################################################");
+                            Logger.WriteLogggerTest(wxKey + ":" + userIP + ":" + result);
+                            Logger.WriteLogggerTest(wxKey + ":" + context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
+
+
+
+
+
+                        }
+                        catch (System.ServiceModel.CommunicationException)
+                        {
+                            //  if (SpVoiceObj != null) SpVoiceObj.Abort();
+                            if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
+                        }
+                        catch (TimeoutException)
+                        {
+                            //   if (SpVoiceObj != null) SpVoiceObj.Abort();
+                            if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
+                        }
+                        catch (Exception ex)
+                        {
+                            //   if (SpVoiceObj != null) SpVoiceObj.Abort();
+                            if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
+                            result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + urlCheck + "\",\"Msg\":\"请求操作在配置的超时,请联系管理员!\"}";
+                            LogTools.WriteLine(userIP + ":" + wxKey + ":" + ex.Message);
+                        }
+
 
                     }
                 }
                 else
                 {
-                    result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + userIP + "\",\"Msg\":\"测试接口,请在每天(08:00-21:00)时间段进行测试,需要讨论技术,联系管理员qq:391502069.\"}";
+                    result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + userIP + "\",\"Msg\":\"参数错误,联系管理员qq:391502069!\"}";
+
                 }
-               
+
+
             }
             else
             {
                 result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"QQ:391502069 \",\"Msg\":\"参数错误,联系管理员qq:391502069!\"}";
             }
             context.Response.Headers.Add("Access-Control-Allow-Origin", "http://www.rrbay.xyz");
-            context.Response.Headers.Add("Access-Control-Allow-Methods", "POST");            
+            context.Response.Headers.Add("Access-Control-Allow-Methods", "POST");
             context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
             context.Response.Write(result);
             context.Response.End();
