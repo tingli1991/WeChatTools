b0c868a4d24b81e3921f73985e417b7245d4e9c0
Gemgin <gemgin@126.com>
2019-10-27 12:24:09 +0800

    免费的给时间控制

--- WeChatTools/WeChatTools.API/pro/wxUrlCheck3.ashx.cs
+++ WeChatTools/WeChatTools.API/pro/wxUrlCheck3.ashx.cs
@@ -19,7 +19,8 @@ namespace WeChatTools.API.pro
         protected const string POST = "POST";
         protected int IsFreeKey = 1;
         private string wxCheckApiKey = ConfigTool.ReadVerifyConfig("wxCheckApiKey3", "CheckKey");
-
+        private TimeSpan _strWorkingDayAM = DateTime.Parse("08:00").TimeOfDay;//工作时间上午08:00
+        private TimeSpan _strWorkingDayPM = DateTime.Parse("21:00").TimeOfDay;
 
         public void ProcessRequest(HttpContext context)
         {
@@ -27,17 +28,9 @@ namespace WeChatTools.API.pro
             string wxKey = wxCheckApiKey; //key ,md5值
             if (context.Request.HttpMethod.ToUpper().Equals(POST))
             {
-                //PostHtml();
-
-               
                 context.Response.ContentType = "text/plain";
-                TimeSpan dspNow = DateTime.Now.TimeOfDay;
-
                 string urlCheck = context.Request["url"];
-
                 string model = context.Request["model"];
-
-            
 
                 if (!string.IsNullOrEmpty(urlCheck) && !string.IsNullOrEmpty(model))
                 {
@@ -46,7 +39,7 @@ namespace WeChatTools.API.pro
                     {
                         wxKey = context.Request["key"]; //key ,md5值
                     }
-                    if (!wxKey.ToLower().Equals(wxCheckApiKey)) 
+                    if (!wxKey.ToLower().Equals(wxCheckApiKey))
                     {
                         IsFreeKey = 0;
                     }
@@ -55,47 +48,54 @@ namespace WeChatTools.API.pro
                         IsFreeKey = 1;
                         userIP = GetWebClientIp(context);
                     }
-
-                    ServiceApiClient SpVoiceObj2 = null;
-                    //    ServiceApiClient SpVoiceObj = null;
-                    try
+                    TimeSpan dspNow = DateTime.Now.TimeOfDay;
+                    if ((IsFreeKey == 1 && IsInTimeInterval(dspNow, _strWorkingDayAM, _strWorkingDayPM)) || IsFreeKey == 0)
                     {
-                        //需要检测的网址                                
-                        bool isTrue = urlCheck.StartsWith("http");
-                        if (!isTrue) { urlCheck = "http://" + urlCheck; }
-                        urlCheck = System.Web.HttpUtility.UrlEncode(urlCheck);
 
-                        string json2 = "{\"Mode\":\"" + model + "\",\"Param\":\"{\'CheckUrl\':\'" + urlCheck + "\',\'UserKey\':\'" + wxKey + "\',\'UserIP\':\'" + userIP + "\',\'IsFreeKey\':'" + IsFreeKey + "'}\"}";
+                        ServiceApiClient SpVoiceObj2 = null;
+                        //    ServiceApiClient SpVoiceObj = null;
+                        try
+                        {
+                            //需要检测的网址                                
+                            bool isTrue = urlCheck.StartsWith("http");
+                            if (!isTrue) { urlCheck = "http://" + urlCheck; }
+                            urlCheck = System.Web.HttpUtility.UrlEncode(urlCheck);
 
-                        SpVoiceObj2 = new ServiceApiClient("NetTcpBinding_IServiceApi");
-                        SpVoiceObj2.Open();
-                        result = SpVoiceObj2.Api(json2);
-                        SpVoiceObj2.Close();
+                            string json2 = "{\"Mode\":\"" + model + "\",\"Param\":\"{\'CheckUrl\':\'" + urlCheck + "\',\'UserKey\':\'" + wxKey + "\',\'UserIP\':\'" + userIP + "\',\'IsFreeKey\':'" + IsFreeKey + "'}\"}";
 
-                        Logger.WriteLogggerTest("#################################################");
-                        Logger.WriteLogggerTest(wxKey + ":" + userIP + ":" + result);
-                        Logger.WriteLogggerTest(wxKey + ":" + context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
+                            SpVoiceObj2 = new ServiceApiClient("NetTcpBinding_IServiceApi");
+                            SpVoiceObj2.Open();
+                            result = SpVoiceObj2.Api(json2);
+                            SpVoiceObj2.Close();
+
+                            Logger.WriteLogggerTest("#################################################");
+                            Logger.WriteLogggerTest(wxKey + ":" + userIP + ":" + result);
+                            Logger.WriteLogggerTest(wxKey + ":" + context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
 
 
+                        }
+                        catch (System.ServiceModel.CommunicationException)
+                        {
+                            //  if (SpVoiceObj != null) SpVoiceObj.Abort();
+                            if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
+                        }
+                        catch (TimeoutException)
+                        {
+                            //   if (SpVoiceObj != null) SpVoiceObj.Abort();
+                            if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
+                        }
+                        catch (Exception ex)
+                        {
+                            //   if (SpVoiceObj != null) SpVoiceObj.Abort();
+                            if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
+                            result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + urlCheck + "\",\"Msg\":\"请求操作在配置的超时,请联系管理员!\"}";
+                            LogTools.WriteLine(userIP + ":" + wxKey + ":" + ex.Message);
+                        }
                     }
-                    catch (System.ServiceModel.CommunicationException)
+                    else
                     {
-                        //  if (SpVoiceObj != null) SpVoiceObj.Abort();
-                        if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
+                        result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + userIP + "\",\"Msg\":\"测试接口,请在每天(08:00-21:00)时间段进行测试,需要讨论技术,联系管理员qq:391502069.\"}";
                     }
-                    catch (TimeoutException)
-                    {
-                        //   if (SpVoiceObj != null) SpVoiceObj.Abort();
-                        if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
-                    }
-                    catch (Exception ex)
-                    {
-                        //   if (SpVoiceObj != null) SpVoiceObj.Abort();
-                        if (SpVoiceObj2 != null) SpVoiceObj2.Abort();
-                        result = "{\"State\":false,\"Code\":\"003\",\"Data\":\"" + urlCheck + "\",\"Msg\":\"请求操作在配置的超时,请联系管理员!\"}";
-                        LogTools.WriteLine(userIP + ":" + wxKey + ":" + ex.Message);
-                    }
-
 
                 }
                 else
